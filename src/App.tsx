9445e3f1f6f7c131c3a246bfbc67776cc3999332
Giedrius Rudzianskas <g.rudzianskas@gmail.com>
2022-07-24 22:00:05 +0100

    add components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { ReducedState } from './redux/types';
-import { fetchCart, changeProductQty, clearCart } from './redux/actions';
-import logo from './logo.svg';
-import './App.css';
+import CartContainer from './containers/cartContainer';
 
 function App() {
-  const dispatch = useDispatch();
-  const products = useSelector(
-    (state: ReducedState) => state.shoppingCart.products
-  );
-  const cartSubTotal = useSelector(
-    (state: ReducedState) => state.shoppingCart.cartSubTotal
-  );
-  const currency = useSelector(
-    (state: ReducedState) => state.shoppingCart.currency
-  );
-
-  useEffect(() => {
-    dispatch(fetchCart());
-  }, [dispatch]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {products?.map((item) => (
-          <input
-            type="number"
-            value={item.qty}
-            min={0}
-            name={String(item.id)}
-            key={item.id}
-            onChange={(e) =>
-              dispatch(
-                changeProductQty({
-                  id: Number(e.target.name),
-                  qty: Number(e.target.value)
-                })
-              )
-            }
-          ></input>
-        ))}
-        <p>{currency + cartSubTotal || 0}</p>
-        <button onClick={() => dispatch(clearCart())}>Clear</button>
-      </header>
+    <div className="mainWrapper">
+      <CartContainer />
     </div>
   );
 }
