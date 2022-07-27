51fdf0635ea6aa9ede04cd910a79a5bcaf2555c6
Giedrius Rudzianskas <g.rudzianskas@gmail.com>
2022-07-27 01:09:14 +0100

    add test-id's aand tests

--- src/containers/cartContainer/index.tsx
+++ src/containers/cartContainer/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReducedState } from '../../redux/types';
 import { fetchCart, changeProductQty, clearCart } from '../../redux/actions';
 import './CartContainer.scss';
 
-function CartContainer() {
+const CartContainer = () => {
   const dispatch = useDispatch();
   const products = useSelector(
     (state: ReducedState) => state.shoppingCart.products
@@ -21,54 +21,73 @@ function CartContainer() {
   }, [dispatch]);
 
   return currency ? (
-    <div className="cartWrapper">
+    <div id="cartWrapper" className="cartWrapper">
       <div className="productWrapper">
-        {products?.map((item) => (
-          <div className="productRow">
-            <h6 className="productTitle">{item.title}</h6>
-            <div className="productNumbers">
-              <input
-                type="number"
-                value={item.qty}
-                min={0}
-                max={99}
-                name={String(item.id)}
-                key={item.id}
-                onFocus={handleFocus}
-                onChange={(e) => {
-                  if (e.target.value.length > 1 && e.target.value[0] === '0') {
-                    handleFocus(e);
-                  }
-                  dispatch(
-                    changeProductQty({
-                      id: Number(e.target.name),
-                      qty: Number(e.target.value)
-                    })
-                  );
-                }}
-              ></input>
-              <h6 className="singleProductTotals">
-                {currency + (item.price * item.qty).toFixed(2)}
+        {products?.map((item, idx) => {
+          const { id, title, qty, price } = item;
+          return (
+            <div data-testId="productRow" key={id} className="productRow">
+              <h6 data-testId={`productTitle_${idx}`} className="productTitle">
+                {title}
               </h6>
+              <div className="productNumbers">
+                <input
+                  data-testId={`quantityInput_${idx}`}
+                  type="number"
+                  value={qty}
+                  min={0}
+                  max={99}
+                  name={String(id)}
+                  onFocus={handleFocus}
+                  onChange={(e) => {
+                    if (
+                      e.target.value.length > 1 &&
+                      e.target.value[0] === '0'
+                    ) {
+                      handleFocus(e);
+                    }
+                    dispatch(
+                      changeProductQty({
+                        id: Number(e.target.name),
+                        qty: Number(e.target.value)
+                      })
+                    );
+                  }}
+                ></input>
+                <h6
+                  data-testId={`productTotals_${idx}`}
+                  className="singleProductTotals"
+                >
+                  {currency + (price * qty).toFixed(2)}
+                </h6>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className="actionWrapper">
-        <h2 className="cartSubTotal">
+        <h2 data-testId="cartSubTotal" className="cartSubTotal">
           {currency + cartSubTotal.toFixed(2) || 0}
         </h2>
         <div className="buttonWrapper">
-          <button className="buttonClear" onClick={() => dispatch(clearCart())}>
+          <button
+            data-testId="buttonClear"
+            className="buttonClear"
+            onClick={() => dispatch(clearCart())}
+          >
             Clear
           </button>
-          <button className="buttonCheckout" disabled={!cartSubTotal}>
+          <button
+            data-testId="buttonCheckout"
+            className="buttonCheckout"
+            disabled={!cartSubTotal}
+          >
             Check Out
           </button>
         </div>
       </div>
     </div>
   ) : null;
-}
+};
 
 export default CartContainer;
---
+++ src/utils/fixtures/shoppingCartResponse.ts
@@ -0,0 +1,23 @@
+export const shoppingCartResponse = {
+  products: [
+    {
+      id: 1,
+      title: 'Mountain Dew',
+      price: 1.8,
+      qty: 1
+    },
+    {
+      id: 2,
+      title: 'Desperados',
+      price: 3.1,
+      qty: 1
+    },
+    {
+      id: 3,
+      title: 'Jack Daniels',
+      price: 3.35,
+      qty: 1
+    }
+  ],
+  currency: '$'
+};
